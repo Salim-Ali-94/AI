08130dcc06d9772a14000b1f2dea810882030e64
Salim Ali <75537889+Salim-Ali-94@users.noreply.github.com>
2021-09-26 19:15:21 +0200

    Add files via upload

---
+++ PyTorch/Neural Network/cnn_test.py
@@ -0,0 +1,23 @@
+import modular_network as network
+
+
+if __name__ == "__main__":
+
+	X, y = network.extract("mnist", "MNIST")
+	Y = [element.item() for element in y]
+	output, window = len(list(set(Y))), 3
+	batch, train_percent = 32, 80
+	episodes, learning_rate = 50, 5e-3
+	trainer, tester, validater = network.partition(X, y, output, batch, train_percent)
+	height, channel = X[0].shape[-1], X[0].shape[0]
+	kernel, stride, padding = [5, 5], [1, 1], [1, 1]
+	convolutions = [channel, 10, 20]
+	activity = ["relu", "relu"]
+	activity += ["relu", ""]
+	neurons = [50, output]
+	cost = "crossentropy"
+	optimizer = "adam"
+	model, error, accuracy = network.learn(trainer, neurons, activity, learning_rate, episodes, cost, optimizer, height, kernel, stride, padding, window, convolutions)
+	network.plot(error, "forestgreen", "accumulated_error_over_each_epoch_mnist_classification_cnn_", "Episode", "Error")
+	network.plot(accuracy, "mediumvioletred", "accuracy_over_each_epoch_mnist_classification_cnn_", "Episode", "Learning accuracy")
+	network.test(model, tester, output, True)
